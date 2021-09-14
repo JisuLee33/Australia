736e887ebe2a436d4e4f68c82666f45978dd0c84
JisuLee <9033jisu@naver.com>
2021-09-14 19:10:40 +0900

    update responsive web.

--- js/menu_toggle.js
+++ js/menu_toggle.js
@@ -1,5 +1,22 @@
 $(function(){
-   $(".fa-bars").hover(function(){
-    $(".nav-list").toggle();
+   $(".fa-bars").on("mouseover",function(){
+    $(".nav-list").show();
    }); 
+   $(".fa-bars").on("mouseout",function(){
+      $(".nav-list").hide();
+     });
+   // if (window.matchMedia("(max-width: 1024px)").matches) {
+   //    $(".nav-list").hide();
+   //    $(".fa-bars").hover(function(){
+   //    $(".nav-list").stop().toggle();
+   //    });
+   // }
+   
+   // $(window).resize(function(){
+   //    if(window.innerWidth>1024){
+   //       $(".nav-list").show();
+   //    } else { 
+   //       $(".nav-list").hide();
+   //    }
+   // });
 });
